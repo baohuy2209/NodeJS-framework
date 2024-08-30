79b05a8f98640a3bb129447feb0ae788e987fcf8
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-08-31 00:14:07 +0700

    body-parser example in Koajs ###

--- KOAJS/koa_example_project/body_parsing/index.js
+++ KOAJS/koa_example_project/body_parsing/index.js
@@ -1,2 +1,21 @@
 const koa = require("koa");
 const app = new koa();
+const { koaBody } = require("koa-body");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+app.use(
+  koaBody({
+    jsonLimit: "1kb",
+  })
+);
+app.use(async function (ctx) {
+  const body = ctx.request.body;
+  if (!body.name) {
+    ctx.throw(400, ".name required");
+  }
+  ctx.body = { name: body.name.toUpperCase() };
+});
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
