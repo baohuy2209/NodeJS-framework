187e53a5f370ad3973795349692e7fe942fde887
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-09-04 20:18:44 +0700

    stream object

--- KOAJS/koa_example_project/stream-objects/index.js
+++ KOAJS/koa_example_project/stream-objects/index.js
@@ -1,2 +1,28 @@
 const Koa = require("koa");
-const JSONStream = require;
+const JSONStream = require("streaming-json-stringify");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || PORT;
+const app = new Koa();
+app.use(async function (ctx) {
+  ctx.type = "json";
+  const stream = JSONStream();
+  ctx.body = JSONStream();
+  stream.on("error", ctx.onerror);
+  setImmediate(function () {
+    stream.write({
+      id: 1,
+    });
+    setImmediate(function () {
+      stream.write({
+        id: 2,
+      });
+      setImmediate(function () {
+        stream.end(); // End the stream
+      });
+    });
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
