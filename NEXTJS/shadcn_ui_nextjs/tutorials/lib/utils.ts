f318a2efa5945921933f38815c1fad1356ab43db
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-10-20 16:27:39 +0700

    page mail example with shadcn\ui ######

--- NEXTJS/shadcn_ui_nextjs/tutorials/lib/utils.ts
+++ NEXTJS/shadcn_ui_nextjs/tutorials/lib/utils.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c8c3dfc0cdd370d44932828eb067ef771c8fe7996693221d5d4b90af6d54f2d
-size 166
+oid sha256:504682a67c70b0b714c19b4bbffb2d1e2c4a02f3a9e1b9c838f4160100dad627
+size 475
