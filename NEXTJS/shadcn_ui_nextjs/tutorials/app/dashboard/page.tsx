49681f827cde521735ef76f2a6b3ddd32429808d
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-11-09 23:54:59 +0700

    app example about next auth

---
+++ NEXTJS/shadcn_ui_nextjs/tutorials/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import {Metadata} from "next"; 
+import Image from "next/image"; 
+import {Button} from "@/components/ui/button"; 
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"; 
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"; 
+import {CalendarDateRangePicker} from "@/components/components_dashboard/date-range-picker"; 
+import MainNav from "@/components/components_dashboard/main-nav", 
+import Overview from "@/components/components_dashboard/overview"; 
+import RecentSales from "@/components/components_dashboard/recent-sales"; 
+import Search from "@/components/components_dashboard/search";
+import TeamSwitcher from "@/components/components_dashboard/team-switcher"; 
+import {UserNav} from  "@/components/components_dashboard/user-nav"; 
