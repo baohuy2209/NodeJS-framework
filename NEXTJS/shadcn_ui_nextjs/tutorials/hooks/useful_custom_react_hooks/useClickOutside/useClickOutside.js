d7b7db44a5b38543a31396c2c254e2c1142c7ece
baohuy2209 <huynguyen002311@gmail.com>
2024-12-19 21:47:56 +0700

    useClickOutside custom hook in JS

---
+++ NEXTJS/shadcn_ui_nextjs/tutorials/hooks/useful_custom_react_hooks/useClickOutside/useClickOutside.js
@@ -0,0 +1,14 @@
+import useEventLister from "../useEventListener/useEventListener";
+
+export default function useClickOutside(ref, cb) {
+  useEventListener(
+    "click",
+    (e) => {
+      if (ref.current == null || ref.current.contains(e.target)) {
+        return;
+      }
+      cb(e);
+    },
+    element
+  );
+}
