afe0f2c1585dd95441a984f2fbbc746ff16beaa3
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2025-04-13 13:43:20 +0700

    dynamic list filter example using Angular

---
+++ ANGULAR/dynamic-list-filter/src/app/app.component.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20f9a1584088240e2eabd1173b62ea8b2bc0a50c3f890ea7301ea15da30fcb69
+size 1129
---
+++ ANGULAR/dynamic-list-filter/src/app/filter.pipe.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6776812e1de3e67ba840d0f3303e21e2031c57f4e60ac3b514cba1287b813918
+size 370
